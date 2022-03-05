5a12cb2eda47646d43a9f5e0bfeeffb8118e9004
hiagogrades <hiagogrades@hotmail.com>
2022-03-05 19:51:29 -0300

    Cria header

---
+++ MembrosEstaticos2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Globalization;
+
+namespace MembrosEstaticos2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Calculadora calc = new Calculadora();
+
+            Console.WriteLine("Entre com o valor do raio: ");
+            double raio = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            double circ = calc.Circunferencia(raio);
+            double volume = calc.Volume(raio);
+
+
+            Console.WriteLine("Circunferência: " + circ.ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine("Volume: " + volume.ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine("Valor do PI: " + calc.Pi.ToString("F2", CultureInfo.InvariantCulture));
+
+        }
+    }
+}
